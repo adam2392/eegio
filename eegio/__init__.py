63951fbe66d075f5b6057f1e8f7f4e32b7522238
Adam Li <adam2392@gmail.com>
2019-10-03 11:46:35 -0400

    Reformatted to black.

--- eegio/__init__.py
+++ eegio/__init__.py
@@ -3,6 +3,7 @@ import glob
 
 from .format.format_eeg_data import FormatEEGData as format_eegdata
 from .format.format_clinical_sheet import format_clinical_sheet
+import eegio.base.objects as objects
 
 modules = glob.glob(dirname(__file__) + "/*.py")
 __all__ = [
